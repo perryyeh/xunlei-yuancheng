77f9814c17423d1a155e7bc288e403db6a772818
Perry Yeh <perryyeh@users.noreply.github.com>
2016-03-22 13:21:10 +0800

    add sync device status and fix bug

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 //config
 var config = {
   //device list
-  "device" : "http://homecloud.yuancheng.xunlei.com/listPeer?type=0&v=2&ct=0",
+  "device" : "http://homecloud.yuancheng.xunlei.com/listPeer?type=0&v=2&ct=0&_=" + Date.parse(new Date()),
   //download url
   "yuancheng" : "http://yc.xunlei.com/?download={download}&pid={pid}"
 }
@@ -10,7 +10,7 @@ var config = {
 function getDevice(){
   var json = [{
     "name" : chrome.i18n.getMessage("defaultdevice"),
-    "pid" : "thunder",
+    "pid" : "",
     "online" : "65535"
   }];
 
@@ -36,14 +36,23 @@ function openYuancheng(info, tab) {
   chrome.tabs.create({"url": url});
 }
 
-var title = chrome.i18n.getMessage("context_title");
-var device = getDevice();
-//add multiple device menu
-//but chrome collapses them to single, f**k
-for (var i = 0; i < device.length; i++) {
-  var menuTitle = title;
-  menuTitle = menuTitle.replace("{device}",device[i].name);
-  menuTitle = menuTitle.replace("{status}", chrome.i18n.getMessage("status"+device[i].online));
-  chrome.contextMenus.create({type: "normal", id: device[i].pid, title: menuTitle, contexts: ["link"], onclick: openYuancheng});
+//manage menu
+function manageMenu(){
+  chrome.contextMenus.removeAll();
+
+  var title = chrome.i18n.getMessage("context_title");
+  var device = getDevice();
+  //add multiple device menu
+  //but chrome collapses them to single, f**k
+  for (var i = 0; i < device.length; i++) {
+    var menuTitle = title;
+    menuTitle = menuTitle.replace("{device}",device[i].name);
+    menuTitle = menuTitle.replace("{status}", chrome.i18n.getMessage("status"+device[i].online));
+    chrome.contextMenus.create({type: "normal", id: device[i].pid, title: menuTitle, contexts: ["link"], onclick: openYuancheng});
+  }
+  //alert(JSON.stringify(device));
 }
-//alert(JSON.stringify(device));
+
+//sync device & status @ 30s
+manageMenu();
+setInterval(manageMenu, 30000);
